39ffee7276086e306ff58567d0f2015f1e81948f
Nathan McCutchen <n8wm@icloud.com>
2017-07-30 19:30:09 -0400

    making it better

---
+++ NewUser.php
@@ -0,0 +1,6 @@
+<?php include('Head.php'); ?>
+<body>
+<?php
+
+?>
+</body>
--- Variables.php
+++ Variables.php
@@ -1,1 +1,2 @@
-
+<?php
+$arr = array("N8WM"=>"0083a5d723f5cf62e38eec0572f76663d91b497f", "mccuttom"=>"179e13144ca36db904f242d1520275d62f79cfc7", "rmccutchen"=>"7271b2084652d5ff3078beb6b9eda57813f462b7");
